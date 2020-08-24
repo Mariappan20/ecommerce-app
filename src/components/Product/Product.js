0d7bcb778ff21363d4dc0ae316b8358012b7621e
mariappan20 <mariappannec20@gmail.com>
2020-08-24 20:11:58 +0530

    list all the products

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,25 +1,20 @@
 import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
 
 import './Product.css';
 
 const Product = (props) => {
 	return (
-		<Container >
-			<Row className="product">
-				<Col md={4} lg={3} xl={3}  className="pad__zero" >
-					<div className="product__card">
-						<div>
-							<img src={`../assets/images/${props.src}.jpg`} height="250" weight="250" />
-						</div>
-						<div>
-							<small>{props.name}</small>
-							<p>&#8377; {props.price}</p>
-						</div>
-					</div>
-				</Col>		
-			</Row>
-		</Container>
+
+		<div className="productCard">
+			<div>
+				<img src={`../assets/images/${props.src}.jpg`} height="250" weight="250" />
+			</div>
+			<div>
+				<small>{props.name}</small>
+				<p>&#8377; {props.price}</p>
+			</div>
+		</div>
+		
 	);
 }
 
--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,16 +1,63 @@
-import React from 'react';
+import React,{ useState } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+
 
 import './Home.css';
 import Product from '../../components/Product/Product';
 
 const Home = (props) => {
+	
+	const [ products,setProducts ] = useState([
+		{ 
+			name: "iPhone 6",
+			price: "70000",
+			src: "apple"
+		},
+		{ 
+			name: "samsung galaxy j6",
+			price: "70000",
+			src: "samsung"
+		},
+		{ 
+			name: "asus xen pro",
+			price: "80000",
+			src: "asus"
+		},
+		{ 
+			name: "asus xen pro",
+			price: "80000",
+			src: "asus"
+		},
+		{ 
+			name: "asus xen pro",
+			price: "80000",
+			src: "asus"
+		},
+		{ 
+			name: "asus xen pro",
+			price: "80000",
+			src: "asus"
+		},
+		
+		
+	]);
 	return (
 				<div className="home">
-					<Product name="iPhone 6" price="70000" src="apple" />	
-					<Product name="samsung galaxy j6" price="7000" src="samsung" />	
-					<Product name="asus xen pro" price="80000" src="asus" />	
-					<Product name="asus xen pro" price="80000" src="apple" />	
-					<Product name="asus xen pro" price="80000" src="apple" />	
+					<Container>
+						<Row>
+								{ 
+									products.map( products => (
+										<Col md={4} lg={3} xl={3}  className="pad__zero" >
+											<Product name={products.name}
+												price={products.price}
+												src={products.src}
+											/>
+										</Col>	
+									) )
+								}	
+						</Row>
+					</Container>
+					
 				</div>
 			);
 }
